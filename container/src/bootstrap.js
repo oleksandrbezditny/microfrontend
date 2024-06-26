70ea328b93794d193a5f2cc1af2fac9721e84012
Oleksandr Bezditnyi <oleksandrbezditny@gmail.com>
2024-06-26 20:38:12 +0200

    products project export mount

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,5 +1,6 @@
-import 'products/ProductsIndex';
+import { mount } from 'products/ProductsIndex';
 import 'cart/CartShow';
 
+mount(document.querySelector('#dev-products-container'));
 console.log('container');
 
